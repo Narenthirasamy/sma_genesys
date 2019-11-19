455be100913afa20e4d3928f349e6f69ff837565
mibrahim <mohammed.ibrahim@genesys.com>
2019-11-19 19:33:01 +0530

    Methods push commit 1
    Methods push , feature reachablitiy
    sentimental analysis

--- feature_reachability.py
+++ feature_reachability.py
@@ -1,56 +1,16 @@
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
 import pandas as pd
-from textblob import TextBlob
-import json
+import statsmodels.api as sm
+import matplotlib
 
-#Calculate percentage
-def percentage(part, whole):
-    return 100 * float(part)/float(whole)
-
-def get_Sentiments()
+def get_FeatureReachability()
 {
-	df = pd.read_excel(r"C:\Users\mibrahim\Desktop\Prediction.xlsx")
-	tweet_contents = df['Tweet content']
+	df = pd.read_excel(r"C:\Users\mibrahim\Desktop\Feature_Prediction.xls", sheet_name='Sheet1')
 
-	#Sentiment Analysis
-	positive = 0
-	negative = 0
-	nuetral = 0
-	polarity = 0
-
-	for tweet in tweet_contents:
-	    #print(tweet)
-	    #print(tweet.user.location)
-	    analysis = TextBlob(tweet)
-	    #print(analysis.sentiment)
-	    polarity+= analysis.sentiment.polarity
-
-	if (analysis.sentiment.polarity == 0.00):
-    	nuetral+=1
-
-	elif (analysis.sentiment.polarity > 0.00):
-    	positive+=1
-
-	elif (analysis.sentiment.polarity < 0.00):
-    	negative+=1
-	noOfItems = 204820
-
-	positive = percentage(positive,noOfItems)
-	negative = percentage(negative,noOfItems)
-	nuetral = percentage(nuetral,noOfItems)
-	polarity =percentage(polarity,noOfItems)
-
-	data = {}
-
-	if (polarity == 0):
-    	data['type'] = 'Neutral'
-	elif (polarity < 0):
-    	data['type'] = 'Negative'
-	elif(polarity > 0):
-    	data['type'] = 'positive'
-
-    date['value'] = polarity
-    json_data = json.dumps(data)
-
-    return json_data
-
+	cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
 }
